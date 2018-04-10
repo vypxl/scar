8fcdc50d2078437a7fc62f0427190a33b3fa8ba8
vypxl <thomas@vypxl.io>
2018-04-10 18:08:16 +0200

    util for writing and reading files, serialization with msgpack, config manager

--- src/scar.cr
+++ src/scar.cr
@@ -1,4 +1,33 @@
+require "crsfml"
+require "chipmunk"
+require "msgpack"
 require "./scar/*"
 
 module Scar
+  extend self
+
+  # Use this instead of initialize if you want a serializable type.
+  # It creates a initialize method and a MessagePack mapping to use it with
+  # `to_msgpack` and `from_msgpack` and getter/setter methods for all fields.
+  # Usage: instead of traditional initialize(),
+  # you specify a NamedTuple with format { <field_name>: <Type>, .. }.
+  # Example:
+  # ```
+  # include Scar
+  # class Person
+  #   serializable({name: String, age: UInt32, friend_names: Array(String)})
+  # end
+  #
+  # # generates
+  #
+  # class Person
+  #   property :name, :age, :friend_names # This already happens when doing the MessagePack.mapping but for completeness sake..
+  #   def initialize(@name : String, @age : UInt32, @friend_names : Array(String))
+  #   MessagePack.mapping({name: String, age: UInt32, friend_names: Array(String)})
+  # end
+  # ```
+  macro serializable(fields)
+    def initialize({% for k, v, i in fields %}@{{k.id}} : {{v.id}}{% if i < fields.size - 1 %}, {% end %}{% end %}); end
+    MessagePack.mapping({{fields}}, strict = true)
+  end
 end
