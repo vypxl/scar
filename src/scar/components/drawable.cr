f9cea2298350e864289fd18dc0a7688cf64b43a3
vypxl <thomas@vypxl.io>
2019-04-29 21:38:35 +0200

    fix format

--- src/scar/components/drawable.cr
+++ src/scar/components/drawable.cr
@@ -1,4 +1,4 @@
 # Components inheriting this must define a getter to a SFML Drawable
 abstract class Scar::Components::Drawable < Scar::Component
-  abstract def sf : SF::Drawable #, but its a module to be included, so..
+  abstract def sf : SF::Drawable
 end
