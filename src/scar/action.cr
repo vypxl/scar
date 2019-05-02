1fca617158274d4348e2c6f757bc0c1561e845df
vypxl <thomas@vypxl.io>
2019-05-02 20:24:05 +0200

    put timed_action into a separate file

--- src/scar/action.cr
+++ src/scar/action.cr
@@ -7,21 +7,4 @@ module Scar
 
     def on_end; end
   end
-
-  # Use this for delayed actions.
-  class TimedAction < Action
-    @time : Float32 = 0
-
-    def initialize(@duration : Float32, @on_end : Proc(Nil))
-    end
-
-    def completed?(dt)
-      @time += dt
-      @time >= @duration
-    end
-
-    def on_end
-      @on_end.call
-    end
-  end
 end
