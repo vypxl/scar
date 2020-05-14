2be2f7ea6a166aa320cef8e5513aea9f683bc50e
vypxl <thomas@vypxl.io>
2020-05-14 18:16:14 +0200

    added Assets.default_font

--- src/scar/components/text.cr
+++ src/scar/components/text.cr
@@ -1,8 +1,9 @@
 class Scar::Components::Text < Scar::Components::Drawable
   getter :text, :font, :sf
 
-  def initialize(@text : String, @font : SF::Font)
-    @sf = SF::Text.new(@text, @font)
+  def initialize(@text : String, font : SF::Font? = nil)
+    font = font || Scar::Assets.default_font || raise "No font given and no default font for text components specified!"
+    @sf = SF::Text.new(@text, font)
   end
 
   def text=(new_text : String)
