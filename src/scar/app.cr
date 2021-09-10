c026cb5279f0554311d1866d6f2de96efabb18be
vypxl <thomas@vypxl.io>
2021-09-10 12:23:07 +0200

    make input a stand-alone module, like Assets This means that all applications have to change any app#input#.. usage to Input#..

--- src/scar/app.cr
+++ src/scar/app.cr
@@ -1,188 +1,186 @@
-module Scar
-  # Base class for Scar powered applications
+# Base class for Scar powered applications
+#
+# Example usage:
+# ```
+# # Define your app
+# class MyApp < Scar::App
+#   @counter = 0
+#
+#   def init
+#     puts "Loading Assets..."
+#     puts "Setting up the scene..."
+#   end
+#
+#   def update
+#     @counter += 1
+#   end
+#
+#   def render
+#     puts "Rendering the scene..."
+#   end
+# end
+#
+# # Create a window to render to
+# window = SF::RenderWindow.new(SF::VideoMode.new(1600, 900), "MyApp", SF::Style::Close)
+#
+# # Instatiate your app
+# app = MyApp.new(window)
+#
+# # Run your app
+# app.run
+# ```
+abstract class Scar::App
+  # Returns the `SF::RenderWindow` of the app.
+  getter :window
+  # Returns the current scenes
+  getter :scene_stack
+  # Returns all currently running actions
+  getter :actions
+
+  # Set this to `true` to enable hot-reloading of assets
+  property hotreload = false
+
+  # App specific initialization
+  #
+  # e. g. loading configuration, binding inputs, loading textures, ..
+  abstract def init
+
+  # App update logic
+  #
+  # This method is executed on every frame **before** all `System` and `Object` update methods.
+  abstract def update(dt)
+
+  # App rendering logic
+  #
+  # This method is executed on every frame **before** all `System` and `Object` rendering methods.
+  abstract def render(dt)
+
+  # Initializes the app with a RenderWindow as a render target
+  def initialize(@window : SF::RenderWindow)
+    @next_id = 0u64
+    @time = Time.utc
+    @scene_stack = Array(Scene).new
+    @tweens = Array(Tween).new
+    @actions = Array(Action).new
+    init()
+  end
+
+  # Starts the app and begins running the event loop
+  def run
+    time = Time.utc
+    while window.open?
+      while e = window.poll_event
+        converted = Event.from_sfml_event(e)
+        broadcast converted if converted
+      end
+
+      new_time = Time.utc
+      dt = (new_time - time).total_seconds
+      Assets.check_hotreload(dt)
+      @tweens.each(&.update dt)
+      @tweens.select! { |t| !t.completed? }
+      @actions.select! { |a| res = a.completed?(dt); a.on_end if res; !res }
+      update dt
+      render dt
+      if @scene_stack.last?
+        @scene_stack.last.update(self, dt)
+        @scene_stack.last.render(self, dt)
+      end
+      @window.display
+      time = new_time
+    end
+  end
+
+  # Unloads assets and exits the program.
+  #
+  # Override this method if you have specific exit logic.
+  # Note that it is recommended that you call super() during you custom exit method.
+  def exit(status = 0)
+    Assets.unload_all
+    window.close
+    window.finalize
+    Process.exit(status)
+  end
+
+  # The following methods are just there for the docs generator,
+  # the actual implementation is inside the `finished` macro later
+
+  # Adds an event handler for the specified event type and returns its id
+  #
+  # The block must have a type of `Proc(event_type, Nil)`.
+  # This method is defined for every subtype of Scar::Event, other types will generate a compiler error.
+  # Exampe usage:
+  # ```
+  # app.subscibe(Scar::Event::Resized) { |evt| puts "Resized!" }
+  # ```
+  def subscribe(event_type, &block) : UInt64
+    {% raise "Invalid event type" %}
+  end
+
+  # Broadcasts the given event (calls all event listeners)
   #
   # Example usage:
   # ```
-  # # Define your app
-  # class MyApp < Scar::App
-  #   @counter = 0
-  #
-  #   def init
-  #     puts "Loading Assets..."
-  #     puts "Setting up the scene..."
-  #   end
-  #
-  #   def update
-  #     @counter += 1
-  #   end
-  #
-  #   def render
-  #     puts "Rendering the scene..."
-  #   end
-  # end
-  #
-  # # Create a window to render to
-  # window = SF::RenderWindow.new(SF::VideoMode.new(1600, 900), "MyApp", SF::Style::Close)
-  #
-  # # Instatiate your app
-  # app = MyApp.new(window)
-  #
-  # # Run your app
-  # app.run
+  # app.broadcast(Scar::Event::Closed.new)
   # ```
-  abstract class App
-    # Returns the `SF::RenderWindow` of the app.
-    getter :window
-    # Returns the input handler of the app.
-    getter :input
-    # Returns the current scenes
-    getter :scene_stack
-    # Returns all currently running actions
-    getter :actions
+  def broadcast(event)
+    raise "Invalid event type"
+  end
 
-    # Set this to `true` to enable hot-reloading of assets
-    property hotreload = false
+  macro finished
+    {% for evt in Event::Event.subclasses %}
+      @event_handlers_{{evt.id.gsub(/::/, "__").id.downcase}} = Hash(UInt64, ({{evt}}->)).new
 
-    # App specific initialization
-    #
-    # e. g. loading configuration, binding inputs, loading textures, ..
-    abstract def init
-
-    # App update logic
-    #
-    # This method is executed on every frame **before** all `System` and `Object` update methods.
-    abstract def update(dt)
-
-    # App rendering logic
-    #
-    # This method is executed on every frame **before** all `System` and `Object` rendering methods.
-    abstract def render(dt)
-
-    # Initializes the app with a RenderWindow as a render target and an input handler
-    def initialize(@window : SF::RenderWindow, @input : Input = Input.new)
-      @next_id = 0u64
-      @time = Time.utc
-      @scene_stack = Array(Scene).new
-      @tweens = Array(Tween).new
-      @actions = Array(Action).new
-      init()
-    end
-
-    # Starts the app and begins running the event loop
-    def run
-      time = Time.utc
-      while window.open?
-        while e = window.poll_event
-          converted = Event.from_sfml_event(e)
-          broadcast converted if converted
-        end
-
-        new_time = Time.utc
-        dt = (new_time - time).total_seconds
-        Assets.check_hotreload(dt)
-        @tweens.each(&.update dt)
-        @tweens.select! { |t| !t.completed? }
-        @actions.select! { |a| res = a.completed?(dt); a.on_end if res; !res }
-        update dt
-        render dt
-        if @scene_stack.last?
-          @scene_stack.last.update(self, dt)
-          @scene_stack.last.render(self, dt)
-        end
-        @window.display
-        time = new_time
+      # :nodoc:
+      def subscribe(event_type : {{evt}}.class, &block : {{evt}}->) : UInt64
+        @event_handlers_{{evt.id.gsub(/::/, "__").id.downcase}}[@next_id] = block
+        @next_id += 1
+        @next_id - 1
       end
-    end
 
-    # Unloads assets and exits the program.
-    #
-    # Override this method if you have specific exit logic.
-    # Note that it is recommended that you call super() during you custom exit method.
-    def exit(status = 0)
-      Assets.unload_all
-      window.close
-      window.finalize
-      Process.exit(status)
-    end
+      # :nodoc:
+      def broadcast(event : {{evt}})
+        @event_handlers_{{evt.id.gsub(/::/, "__").id.downcase}}.each do |_, handler|
+          handler.call(event)
+        end
+      end
+    {% end %} # End macro-for
 
-    # The following methods are just there for the docs generator,
-    # the actual implementation is inside the `finished` macro later
-
-    # Adds an event handler for the specified event type and returns its id
-    #
-    # The block must have a type of `Proc(event_type, Nil)`.
-    # This method is defined for every subtype of Scar::Event, other types will generate a compiler error.
-    # Exampe usage:
-    # ```
-    # app.subscibe(Scar::Event::Resized) { |evt| puts "Resized!" }
-    # ```
-    def subscribe(event_type, &block) : UInt64
-      {% raise "Invalid event type" %}
-    end
-
-    # Broadcasts the given event (calls all event listeners)
-    #
-    # Example usage:
-    # ```
-    # app.broadcast(Scar::Event::Closed.new)
-    # ```
-    def broadcast(event)
-      raise "Invalid event type"
-    end
-
-    macro finished
+    # Deletes the event handler with the given id
+    def unsubscribe(id)
       {% for evt in Event::Event.subclasses %}
-        @event_handlers_{{evt.id.gsub(/::/, "__").id.downcase}} = Hash(UInt64, ({{evt}}->)).new
-
-        # :nodoc:
-        def subscribe(event_type : {{evt}}.class, &block : {{evt}}->) : UInt64
-          @event_handlers_{{evt.id.gsub(/::/, "__").id.downcase}}[@next_id] = block
-          @next_id += 1
-          @next_id - 1
+        if @event_handlers_{{evt.id.gsub(/::/, "__").id.downcase}}[id]?
+          @event_handlers_{{evt.id.gsub(/::/, "__").id.downcase}}.delete id
         end
-
-        # :nodoc:
-        def broadcast(event : {{evt}})
-          @event_handlers_{{evt.id.gsub(/::/, "__").id.downcase}}.each do |_, handler|
-            handler.call(event)
-          end
-        end
-      {% end %} # End macro-for
-
-      # Deletes the event handler with the given id
-      def unsubscribe(id)
-        {% for evt in Event::Event.subclasses %}
-          if @event_handlers_{{evt.id.gsub(/::/, "__").id.downcase}}[id]?
-            @event_handlers_{{evt.id.gsub(/::/, "__").id.downcase}}.delete id
-          end
-        {% end %}
-      end
-    end # End macro finished
-
-    # Push a scene onto the scene stack
-    delegate :<<, to: @scene_stack
-    # Pop a scene from the scene stack
-    delegate pop, to: @scene_stack
-
-    # Registers a `Tween` which is then updated on every frame.
-    #
-    # The `Tween` will be deleted when `Tween#completed?` returns true after an update.
-    # If the Tween restarts itself in `Tween#on_completed` (e. g. by calling `Tween#reset`)
-    # or does anything that prevents the `Tween#completed?` check, it will not be deleted.
-    def tween(t : Tween)
-      @tweens << t
-      t
+      {% end %}
     end
+  end # End macro finished
 
-    # Begin running an `Action`
-    def act(action : Action)
-      @actions << action
-      action.on_start
-    end
+  # Push a scene onto the scene stack
+  delegate :<<, to: @scene_stack
+  # Pop a scene from the scene stack
+  delegate pop, to: @scene_stack
 
-    # Returns the topmost scene on the scene stack (convenience method)
-    def scene
-      @scene_stack.last
-    end
-  end # End class App
-end   # End module Scar
+  # Registers a `Tween` which is then updated on every frame.
+  #
+  # The `Tween` will be deleted when `Tween#completed?` returns true after an update.
+  # If the Tween restarts itself in `Tween#on_completed` (e. g. by calling `Tween#reset`)
+  # or does anything that prevents the `Tween#completed?` check, it will not be deleted.
+  def tween(t : Tween)
+    @tweens << t
+    t
+  end
+
+  # Begin running an `Action`
+  def act(action : Action)
+    @actions << action
+    action.on_start
+  end
+
+  # Returns the topmost scene on the scene stack (convenience method)
+  def scene
+    @scene_stack.last
+  end
+end
+
+# TODO change @window to @target and make it a rendertarget instead of a renderwindow
--- src/scar/input.cr
+++ src/scar/input.cr
@@ -1,8 +1,6 @@
 require "crsfml"
 
-# TODO: make Input abstract and provide a default implementation, so that App's constructor argument makes sense OR remove that from App and make this a static module like Assets
-
-# A simple class to be used in conjunction with `App`, which provides simple digital and analogue input bindings.
+# This module provides simple digital and analogue input bindings.
 #
 # You can also get creative with your input sources, you could e. g. implement an analogue input that changes with the current time.
 #
@@ -13,15 +11,17 @@ require "crsfml"
 #
 # input.axis :time # => 0.415 (example)
 # ```
-class Scar::Input
-  @digital_bindings = Hash(Symbol, Array(-> Bool)).new
-  @analog_bindings = Hash(Symbol, -> Float32).new
+module Scar::Input
+  extend self
+
+  @@digital_bindings = Hash(Symbol, Array(-> Bool)).new
+  @@analog_bindings = Hash(Symbol, -> Float32).new
 
   # Returns true if the specified digital input is currently active
   def active?(which : Symbol)
     ret = false
-    if @digital_bindings[which]?
-      @digital_bindings[which].each { |check| ret = true if check.call }
+    if @@digital_bindings[which]?
+      @@digital_bindings[which].each { |check| ret = true if check.call }
     else
       Logger.warn "No digital input symbol '#{which}'"
     end
@@ -31,8 +31,8 @@ class Scar::Input
   # Returns the value for the specified analogue input
   def axis(which : Symbol)
     ret = 0f32
-    if @analog_bindings[which]?
-      ret = @analog_bindings[which].call
+    if @@analog_bindings[which]?
+      ret = @@analog_bindings[which].call
     else
       Logger.warn "No axis input symbol '#{which}'"
     end
@@ -48,16 +48,16 @@ class Scar::Input
   # input.bind_digital :jump { SF::Keyboard.key_pressed? SF::Keyboard::Space }
   # ```
   def bind_digital(which : Symbol, &block : -> Bool)
-    @digital_bindings[which] = Array(-> Bool).new if !@digital_bindings[which]?
-    @digital_bindings[which] << block
+    @@digital_bindings[which] = Array(-> Bool).new if !@@digital_bindings[which]?
+    @@digital_bindings[which] << block
   end
 
   # Binds a `Symbol` to an analogue input
   #
   # Similar to `#bind_digital`, but here the check has to return a float instead of a boolean
   def bind_axis(which : Symbol, &block : -> Float32)
-    @analog_bindings[which] = Array(-> Bool).new if !@analog_bindings[which]?
-    @analog_bindings[which] = block
+    @@analog_bindings[which] = Array(-> Bool).new if !@@analog_bindings[which]?
+    @@analog_bindings[which] = block
   end
 
   # Shortcut for digital inputs based on keys
